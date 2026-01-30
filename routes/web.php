0d298d3e057a7fa9c1bfdee45d388345fce4a949
apyogananta <evoyege@gmail.com>
2026-01-30 22:21:36 +0700

    feat: add initial web routes for welcome page and debug endpoint.

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/debug', function () {
+    return response()->json([
+        'uri' => request()->getRequestUri(),
+        'path' => request()->path(),
+        'full' => request()->fullUrl()
+    ]);
+});
